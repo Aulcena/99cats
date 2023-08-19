206d3daca71336e590bbd3b9dad057b15c0a4e5f
AJU <aulcena@gmail.com>
2023-08-18 21:42:21 -0400

    bug fix

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -11,16 +11,17 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
-require 'action_view'
+require "action_view"
 class Cat < ApplicationRecord
 
-    include ActionView::Helpers::DateHelper 
+    include ActionView::Helpers::DateHelper
 
-    CAT_COLORS = %w(Red Blue White Black)
+    CAT_COLORS = %w(black white gray yellow)
 
-    validates :birth_date, :color, :name, :sex, pressence: true
-    validates :color, inclusion: { in: CAT_COLORS }
-    validates :sex, inclusion: { in: w%(M F) }
+    validates :birth_date, :color, :name, :sex, presence: true
+    validates :color, inclusion: CAT_COLORS   
+    validates :sex, inclusion: %w(M F)
+    validate :birth_date_cannot_be_future
 
     def birth_date_cannot_be_future
 
