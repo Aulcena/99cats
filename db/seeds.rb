ba6911e27632406a0707f524e575210f835bb5ba
AJU <aulcena@gmail.com>
2023-08-19 01:10:26 -0400

    added seeds for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+ApplicationRecord.transaction do
+
+    # These `destroy_all` commands are not necessary if you use `rails
+    # db:seed:replant`
+    puts 'Destroying tables...'
+   
+    Cat.destroy_all
+    
+    puts 'Resetting id sequences...'
+
+    %w(cats).each do |table_name|
+        ApplicationRecord.connection.reset_pk_sequence!(table_name)
+    end
+
+    cat_1 = Cat.create!(name: "John", birth_date: "1990-10-11", color: "black", sex: "M", description: "I'm the best!")
+    cat_2 = Cat.create!(name: "Rando", birth_date: "1990-02-11", color: "gray", sex: "F", description: "I'm pretty")
+    cat_3 = Cat.create!(name: "Martian", birth_date: "1986-06-05", color: "white", sex: "M", description: "I'm ugly")
+
+    puts "Datebase populated"
+end
